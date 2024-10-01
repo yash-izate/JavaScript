24e3ee2749b818dd2ad22223f8600202be50ce20
Yash Izate <yashizate.softech@gmail.com>
2024-10-01 19:07:29 +0530

    assignment 8

---
+++ Assignments/assign8.js
@@ -0,0 +1,9 @@
+let string  = "ApnaCollege";
+
+console.log(string.slice(4,9));
+console.log(string.indexOf("na"));
+console.log(string.replace("Apna", "Our"));
+console.log(string.repeat(3));
+
+let newStr1 = string.slice(4);
+let newStr2 = console.log(newStr1.replace("l","t").replace("l","t"));
