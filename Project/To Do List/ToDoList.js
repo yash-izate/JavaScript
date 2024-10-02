496f403109a334945257fdacaee66bd135be02f6
Yash Izate <yashizate.softech@gmail.com>
2024-10-02 18:35:14 +0530

    to do list

--- Project/To Do List/ToDoList.js
+++ Project/To Do List/ToDoList.js
@@ -1,30 +1,38 @@
 let todo = [];
 
-let req = prompt("Please enter your request");
+const taskInput = document.getElementById('task');
+const taskList = document.getElementById('taskList');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const deleteTaskBtn = document.getElementById('deleteTaskBtn');
 
-while (true) {
-  if (req == "quit") {
-    console.log("Quiting App ! See you soon.");
-    break;
-  }
-
-  if (req == "list") {
-    for (let i = 0; i < todo.length; i++) {
-      console.log("------------------------------");
-      console.log(task);
-    }
-    console.log("--------------------------------");
-  } else if (req == "add") {
-    let task = prompt("Please enter the task you want add");
+// Add task event
+addTaskBtn.addEventListener('click', () => {
+  const task = taskInput.value.trim();
+  if (task) {
     todo.push(task);
-    console.log("Task Added !");
-  } else if (req = "delete") {
-    let idx = prompt("Please enter the task index");
-    todo.splice(idx,1);
-    console.log("Task Deleted !");
+    updateTaskList();
+    taskInput.value = ''; // Clear input field
   } else {
-    console.log("Wrong Request");
+    alert('Please enter a task.');
   }
+});
 
-  req = prompt("Please enter youer request");
+// Delete the last task event
+deleteTaskBtn.addEventListener('click', () => {
+  if (todo.length > 0) {
+    todo.pop();
+    updateTaskList();
+  } else {
+    alert('No tasks to delete.');
+  }
+});
+
+// Function to update the task list display
+function updateTaskList() {
+  taskList.innerHTML = ''; // Clear the current list
+  todo.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.textContent = task;
+    taskList.appendChild(li);
+  });
 }
