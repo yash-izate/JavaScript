ff1de08519619ddf16f3a4e9d706c7232d755a37
Yash Izate <yashizate.softech@gmail.com>
2024-10-02 13:46:59 +0530

    assignment 14

---
+++ Assignments/assign15.js
@@ -0,0 +1,8 @@
+let array = [3, 6, 2, 9, 5];
+element = 1;
+
+if (array.indexOf(element) != -1) {
+  console.log("element found at index ", array.indexOf(element));
+} else {
+  console.log("Element not found");
+}
