2378a2430be4c9459710a891f198ca9e62026038
Yash Izate <yashizate.softech@gmail.com>
2024-10-12 16:58:23 +0530

    added dom practice

---
+++ KG Coding/script3.js
@@ -0,0 +1,8 @@
+let newBtn = document.createElement("button");
+
+newBtn.innerText = "Click me!";
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").append(newBtn);
+
