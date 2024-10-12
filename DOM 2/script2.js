f77b054ccc8a51112674be21780593608435b070
Yash Izate <yashizate.softech@gmail.com>
2024-10-12 21:03:53 +0530

    dom assign 2

---
+++ DOM 2/script2.js
@@ -0,0 +1,11 @@
+let input = document.createElement("input");
+let btn = document.createElement("button");
+
+btn.innerText = "Click Me";
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(btn);
+
+btn.classList.add("button");
+input.classList.add("input");
+
