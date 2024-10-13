8f1a377c4bcf39111437c6da6ed86d2d4d313b82
Yash Izate <yashizate.softech@gmail.com>
2024-10-13 10:40:40 +0530

    Dom 2 index 3 updated

---
+++ DOM 2/script3.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+
+function changeColor() {
+  console.dir(this.innerText);
+  this.style.backgroundColor = "lightpink";
+}
+
+btn.addEventListener("click", changeColor);
+p.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor);
+h3.addEventListener("click", changeColor);
